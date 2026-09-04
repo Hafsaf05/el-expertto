df53f3653630fe09eeb21bcb50a52c482a6c862b
Hafsa Fathima <hafsahffathima05@gmail.com>
2026-09-04 23:22:20 +0530

    configure changes for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
@@ -16,5 +17,6 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
     nitro(),
+    netlify(),
   ],
 });
